4c7bb653f3541c1cbd093a463d44aa83e8c86a63
vinkharisma <vinkharismadzania@gmail.com>
2024-06-02 12:20:11 +0700

    add viewportWidth, viewportHeight

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
