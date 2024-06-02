33a1eb31cbed3279fa8036e6b41d21d07e125e8d
vinkharisma <vinkharismadzania@gmail.com>
2024-06-02 11:57:59 +0700

    add search user testing

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -85,6 +85,27 @@ describe('Users Management', () => {
         cy.get('button').contains('OK').click()
     })
 
+    it('PPIC can search the user', () => {
+        cy.visit('http://localhost:8000/login')
+
+        cy.get('input[name=email]').type('superadmin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Login').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+        cy.get('a').contains('Users Management').click()
+        cy.get('a').contains('User List').click()
+        cy.url().should('contain', 'http://localhost:8000/user-management/user')
+
+        cy.get('a').contains('Search User').click()
+        cy.url().should('contain', 'http://localhost:8000/user-management/user')
+
+        cy.get('input[name=name]').type('Vice')
+
+        cy.get('button').contains('Submit').click()
+    })
+
     it('VP can open the user page', () => {
         cy.visit('http://localhost:8000/login')
 
@@ -169,5 +190,26 @@ describe('Users Management', () => {
 
         cy.get(':nth-child(5) > .text-center > .d-flex > .ml-2 > .btn').click()
         cy.get('button').contains('OK').click()
+    })
+
+    it('VP can search the user', () => {
+        cy.visit('http://localhost:8000/login')
+
+        cy.get('input[name=email]').type('user@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Login').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+        cy.get('a').contains('Users Management').click()
+        cy.get('a').contains('User List').click()
+        cy.url().should('contain', 'http://localhost:8000/user-management/user')
+
+        cy.get('a').contains('Search User').click()
+        cy.url().should('contain', 'http://localhost:8000/user-management/user')
+
+        cy.get('input[name=name]').type('Vice')
+
+        cy.get('button').contains('Submit').click()
     })
 })
