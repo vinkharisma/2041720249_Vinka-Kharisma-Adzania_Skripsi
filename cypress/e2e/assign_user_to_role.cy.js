ea7364da7b9f1514b6f3b14395557b72ba56eb71
vinkharisma <vinkharismadzania@gmail.com>
2024-06-05 00:48:42 +0700

    add user to role testing

---
+++ cypress/e2e/assign_user_to_role.cy.js
@@ -0,0 +1,15 @@
+describe('Assign User To Role', () => {
+    it('PPIC can open the assign user to role page', () => {
+        cy.visit('http://localhost:8000/login')
+
+        cy.get('input[name=email]').type('superadmin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Login').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+        cy.get('a').contains('Role Management').click()
+        cy.get('a').contains('User To Role').click()
+        cy.url().should('contain', 'http://localhost:8000/role-and-permission/assign-user')
+    })
+})
