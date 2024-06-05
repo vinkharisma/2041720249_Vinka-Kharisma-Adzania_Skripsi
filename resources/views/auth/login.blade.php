fac4fc2a4fba99be0228fd6c07cba0ca03df22a2
vinkharisma <vinkharismadzania@gmail.com>
2024-06-06 01:03:06 +0700

    edit background login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,8 +23,8 @@
             <div class="d-flex flex-wrap align-items-stretch">
                 <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                 <div class="p-4 m-3">
-                    <img src="../assets/img/stisla-fill.svg" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
-                    <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Stisla</span></h4>
+                    <img src="../assets/img/pallet.png" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
+                    <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Wood Pallet Stock Prediction</span></h4>
                     <p class="text-muted">Before you get started, you must login or register if you don't already have an account.</p>
                     <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                     @csrf
@@ -81,14 +81,16 @@
                     </div>
                 </div>
                 </div>
-                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/unsplash/login-bg.jpg">
-                <div class="absolute-bottom-left index-2">
+                {{-- <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/unsplash/login-bg.jpg"> --}}
+                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/background.png">
+                {{-- <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/background1.png"> --}}
+                    <div class="absolute-bottom-left index-2">
                     <div class="text-light p-5 pb-2">
                     <div class="mb-5 pb-3">
                         <h1 id="greeting" class="mb-2 display-4 font-weight-bold"></h1>
                         <h5 class="font-weight-normal text-muted-transparent">Indonesia</h5>
                     </div>
-                    Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+                    {{-- Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a> --}}
                     </div>
                 </div>
                 </div>
