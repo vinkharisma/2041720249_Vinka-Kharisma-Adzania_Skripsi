325591eda906d8f78573d2494b7789dd7c5f0387
vinkharisma <vinkharismadzania@gmail.com>
2024-06-09 12:17:40 +0700

    edit  permission management testing

--- cypress/e2e/permission_management.cy.js
+++ cypress/e2e/permission_management.cy.js
@@ -1,4 +1,4 @@
-describe('Permission Management', () => {
+describe('PPIC - Permission Management', () => {
     it('PPIC can open the permission page', () => {
         cy.visit('http://localhost:8000/login')
 
@@ -12,7 +12,32 @@ describe('Permission Management', () => {
         cy.get('a').contains('Permission List').click()
         cy.url().should('contain', 'http://localhost:8000/role-and-permission/permission')
     })
+})
 
+describe('PPIC - Search Permission List', () => {
+    it('PPIC can search the permission', () => {
+        cy.visit('http://localhost:8000/login')
+
+        cy.get('input[name=email]').type('superadmin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Login').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+        cy.get('a').contains('Role Management').click()
+        cy.get('a').contains('Permission List').click()
+        cy.url().should('contain', 'http://localhost:8000/role-and-permission/permission')
+
+        cy.get('a').contains('Search Permission').click()
+        cy.url().should('contain', 'http://localhost:8000/role-and-permission/permission')
+
+        cy.get('input[name=name]').type('management')
+
+        cy.get('button').contains('Submit').click()
+    })
+})
+
+describe('PPIC - CRUD Permission List', () => {
     it('PPIC can create a permission', () => {
         cy.visit('http://localhost:8000/login')
 
@@ -72,29 +97,7 @@ describe('Permission Management', () => {
 
         cy.get(':nth-child(7) > .page-link').click()
 
-        cy.get(':nth-child(3) > .text-center > .d-flex > .ml-2 > .btn').click()
-
+        cy.get('button').contains('Delete').click()
         cy.get('button').contains('OK').click()
-    })
-
-    it('PPIC can search the permission', () => {
-        cy.visit('http://localhost:8000/login')
-
-        cy.get('input[name=email]').type('superadmin@gmail.com')
-        cy.get('input[name=password]').type('password')
-        cy.get('button').contains('Login').click()
-        cy.url().should('contain', 'http://localhost:8000/dashboard')
-
-        cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
-        cy.get('a').contains('Role Management').click()
-        cy.get('a').contains('Permission List').click()
-        cy.url().should('contain', 'http://localhost:8000/role-and-permission/permission')
-
-        cy.get('a').contains('Search Permission').click()
-        cy.url().should('contain', 'http://localhost:8000/role-and-permission/permission')
-
-        cy.get('input[name=name]').type('management')
-
-        cy.get('button').contains('Submit').click()
     })
 })
